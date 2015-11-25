763246ad0d5832edcd074b2ac39eebafa7b80951
Sven-Michael Stübe <stuebe2k7@gmail.com>
2015-11-25 22:44:58 +0100

    added code comments for the interface

--- src/SMS.Fingerprint.Abstractions/IFingerprint.cs
+++ src/SMS.Fingerprint.Abstractions/IFingerprint.cs
@@ -5,8 +5,26 @@ namespace SMS.Fingerprint.Abstractions
 {
     public interface IFingerprint
     {
+        /// <summary>
+        /// Checks the availability of fingerprint authentication.
+        /// Possible Reasons for unavailability:
+        /// - Device has no sensor
+        /// - OS does not support this functionality
+        /// - Fingerprint is not enrolled
+        /// </summary>
         bool IsAvailable { get; }
+
+        /// <summary>
+        /// Requests the authentication.
+        /// </summary>
+        /// <param name="reason">Reason for the fingerprint authentication request. Displayed to the user.</param>
+        /// <returns></returns>
         Task<FingerprintAuthenticationResult> AuthenticateAsync(string reason);
+
+        /// <summary>
+        /// Requests the authentication (cancelable).
+        /// </summary>
+        /// <see cref="AuthenticateAsync(string)"/>
         Task<FingerprintAuthenticationResult> AuthenticateAsync(string reason, CancellationToken cancellationToken);
     }
 }
