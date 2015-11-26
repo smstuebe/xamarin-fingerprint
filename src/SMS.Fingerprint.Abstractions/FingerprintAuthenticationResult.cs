91b4c032290bc6f9c91b8f5fdf9126545509253f
Sven-Michael Stübe <stuebe2k7@gmail.com>
2015-11-27 00:27:07 +0100

    and again. VS did not save -.-

--- src/SMS.Fingerprint.Abstractions/FingerprintAuthenticationResult.cs
+++ src/SMS.Fingerprint.Abstractions/FingerprintAuthenticationResult.cs
@@ -2,8 +2,19 @@
 {
     public class FingerprintAuthenticationResult
     {
+        /// <summary>
+        /// Indicatates whether the authentication was successful or not.
+        /// </summary>
         public bool Authenticated { get { return Status == FingerprintAuthenticationResultStatus.Succeeded; } }
+
+        /// <summary>
+        /// Detailed information of the authentication.
+        /// </summary>
         public FingerprintAuthenticationResultStatus Status { get; set; }
+
+        /// <summary>
+        /// Reason for the unsucessful authentication.
+        /// </summary>
         public string ErrorMessage { get; set; }
     }
 }
