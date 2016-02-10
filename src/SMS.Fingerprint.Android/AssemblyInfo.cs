8b31ca191a91ee5b4af8dcb4648c83542fd3ca1f
Sven-Michael Stübe <stuebe2k7@gmail.com>
2016-02-10 23:39:23 +0100

    Updated Version to 1.0.0

--- src/SMS.Fingerprint.Android/AssemblyInfo.cs
+++ src/SMS.Fingerprint.Android/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
