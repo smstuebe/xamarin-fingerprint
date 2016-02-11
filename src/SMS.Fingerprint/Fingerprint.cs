140ad4cbfe9d861159f2215a4d36acd51f14db84
Sven-Michael Stübe <stuebe2k7@gmail.com>
2016-02-12 00:03:06 +0100

    added dummy implementation for unsupported platforms

--- src/SMS.Fingerprint/Fingerprint.cs
+++ src/SMS.Fingerprint/Fingerprint.cs
@@ -1,51 +1,25 @@
-﻿/*
-* From Xamarin Plugin Template from James Montemagno @JamesMontemagno
-*/
-
-using System;
+﻿using System;
+using System.Threading;
 using SMS.Fingerprint.Abstractions;
 
 namespace SMS.Fingerprint
 {
     public partial class Fingerprint
     {
-        private static Lazy<IFingerprint> _implementation = new Lazy<IFingerprint>(CreateFingerprint, System.Threading.LazyThreadSafetyMode.PublicationOnly);
-
-        public static IFingerprint Current
-        {
-            get
-            {
-                var ret = _implementation.Value;
-                if (ret == null)
-                {
-                    throw NotImplementedInReferenceAssembly();
-                }
-                return ret;
-            }
-        }
+        private static Lazy<IFingerprint> _implementation = new Lazy<IFingerprint>(CreateFingerprint, LazyThreadSafetyMode.PublicationOnly);
+        public static IFingerprint Current => _implementation.Value;
 
         static IFingerprint CreateFingerprint()
         {
-#if PORTABLE
-            return null;
-#else
             return new FingerprintImplementation();
-#endif
-        }
-
-        internal static Exception NotImplementedInReferenceAssembly()
-        {
-            return new NotImplementedException("This functionality is not implemented in the portable version of this assembly. You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
         }
 
         public static void Dispose()
         {
             if (_implementation != null && _implementation.IsValueCreated)
             {
-                //Implementation.Value.Dispose();
-                _implementation = new Lazy<IFingerprint>(CreateFingerprint, System.Threading.LazyThreadSafetyMode.PublicationOnly);
+                _implementation = new Lazy<IFingerprint>(CreateFingerprint, LazyThreadSafetyMode.PublicationOnly);
             }
         }
     }
-
 }
---
+++ src/SMS.Fingerprint/FingerprintImplementation.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using SMS.Fingerprint.Abstractions;
+
+namespace SMS.Fingerprint
+{
+    internal class FingerprintImplementation : IFingerprint
+    {
+        public bool IsAvailable { get; } = false;
+        public Task<FingerprintAuthenticationResult> AuthenticateAsync(string reason)
+        {
+            return AuthenticateAsync(reason, CancellationToken.None);
+        }
+
+        public Task<FingerprintAuthenticationResult> AuthenticateAsync(string reason, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new FingerprintAuthenticationResult
+            {
+                Status = FingerprintAuthenticationResultStatus.NotAvailable,
+                ErrorMessage = "Not implemented for the current platform"
+            });
+        }
+    }
+}
