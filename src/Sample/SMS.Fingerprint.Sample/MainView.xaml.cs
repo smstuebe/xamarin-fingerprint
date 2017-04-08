41f7448d75a2732b94663da8a819b444a4b7cf3d
Bruce Wilkins <bruce.wayne.wilkins@ing.nl>
2017-04-08 12:43:09 +0200

    Updating sample to include key chain based sample to add, remove, get values.

--- src/Sample/SMS.Fingerprint.Sample/MainView.xaml.cs
+++ src/Sample/SMS.Fingerprint.Sample/MainView.xaml.cs
@@ -3,12 +3,15 @@ using System.Threading;
 using System.Threading.Tasks;
 using Plugin.Fingerprint.Abstractions;
 using Xamarin.Forms;
+using System.Collections.Generic;
 
 namespace SMS.Fingerprint.Sample
 {
     public partial class MainView : ContentPage
     {
         private CancellationTokenSource _cancel;
+        private string _serviceId = Guid.NewGuid().ToString();
+        private string _secureValueKey = "Password";
 
         public MainView()
         {
@@ -18,7 +21,7 @@ namespace SMS.Fingerprint.Sample
         private async void OnAuthenticate(object sender, EventArgs e)
         {
             _cancel = swAutoCancel.IsToggled ? new CancellationTokenSource(TimeSpan.FromSeconds(10)) : new CancellationTokenSource();
-            lblStatus.Text = "";
+            lblAuthenticateStatus.Text = "";
             var result = await Plugin.Fingerprint.CrossFingerprint.Current.AuthenticateAsync("Prove you have fingers!", _cancel.Token);
 
             await SetResultAsync(result);
@@ -27,7 +30,7 @@ namespace SMS.Fingerprint.Sample
         private async void OnAuthenticateLocalized(object sender, EventArgs e)
         {
             _cancel = swAutoCancel.IsToggled ? new CancellationTokenSource(TimeSpan.FromSeconds(10)) : new CancellationTokenSource();
-            lblStatus.Text = "";
+            lblAuthenticateStatus.Text = "";
 
             var dialogConfig = new AuthenticationRequestConfiguration("Beweise, dass du Finger hast!")
             {
@@ -48,8 +51,32 @@ namespace SMS.Fingerprint.Sample
             }
             else
             {
-                lblStatus.Text = $"{result.Status}: {result.ErrorMessage}";
+                lblAuthenticateStatus.Text = $"{result.Status}: {result.ErrorMessage}";
             }
+        }
+
+        private async void OnAddSecureValue(object sender, EventArgs e)
+        {
+            var result = await Plugin.Fingerprint.CrossFingerprint.Current.SetSecureValue(
+                _serviceId, new KeyValuePair<string, string>(_secureValueKey, entSecureValue.Text));
+
+            lblSecureValueStatus.Text = $"{result.Status}: {result.ErrorMessage}";
+        }
+
+        private async void OnRemoveSecureValue(object sender, EventArgs e)
+        {
+            var result = await Plugin.Fingerprint.CrossFingerprint.Current.RemoveSecureValue(
+                _serviceId, _secureValueKey);
+
+            lblSecureValueStatus.Text = $"{result.Status}: {result.ErrorMessage}";
+        }
+
+        private async void OnGetSecureValue(object sender, EventArgs e)
+        {
+            var result = await Plugin.Fingerprint.CrossFingerprint.Current.GetSecureValue(
+               _serviceId, _secureValueKey, "Please authenticate with your fingerprint to continue.");
+
+            lblSecureValueStatus.Text = $"{result.Status}: {result.ErrorMessage}";
         }
     }
 }
