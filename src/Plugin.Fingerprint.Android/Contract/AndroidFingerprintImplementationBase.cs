f3974a2ab0c9e4ce300a43003387fd268c6ff635
Khugan Shanmugeswaran <ks.kshan@gmail.com>
2017-05-30 20:54:54 -0400

    Wired up DeviceAuthImpl with AndroidFingerprintImpl

--- src/Plugin.Fingerprint.Android/Contract/AndroidFingerprintImplementationBase.cs
+++ src/Plugin.Fingerprint.Android/Contract/AndroidFingerprintImplementationBase.cs
@@ -9,15 +9,47 @@ namespace Plugin.Fingerprint.Contract
     /// </summary>
     public abstract class AndroidFingerprintImplementationBase : FingerprintImplementationBase, IAndroidFingerprintImplementation
     {
+        private readonly DeviceAuthImplementation _deviceAuth;
+
+        public AndroidFingerprintImplementationBase()
+        {
+            _deviceAuth = new DeviceAuthImplementation();
+        }
+
         protected override async Task<FingerprintAuthenticationResult> NativeAuthenticateAsync(AuthenticationRequestConfiguration authRequestConfig, CancellationToken cancellationToken = default(CancellationToken))
         {
-            if (authRequestConfig.UseDialog)
+            FingerprintAuthenticationResult result = new FingerprintAuthenticationResult { Status = FingerprintAuthenticationResultStatus.NotAvailable };
+
+            if (await GetAvailabilityAsync() == FingerprintAvailability.Available)
             {
-                var fragment = CrossFingerprint.CreateDialogFragment();
-                return await fragment.ShowAsync(authRequestConfig, this, cancellationToken);
+				if (authRequestConfig.UseDialog)
+				{
+					var fragment = CrossFingerprint.CreateDialogFragment();
+					result = await fragment.ShowAsync(authRequestConfig, this, cancellationToken);
+				}
+				else
+				{
+					result = await AuthenticateNoDialogAsync(new DeafAuthenticationFailedListener(), cancellationToken);
+				}
             }
 
-            return await AuthenticateNoDialogAsync(new DeafAuthenticationFailedListener(), cancellationToken);
+            if(authRequestConfig.AllowAlternativeAuthentication && result.Status != FingerprintAuthenticationResultStatus.Succeeded)
+            {
+                if(_deviceAuth.IsDeviceAuthSetup())
+                {
+                    result = await _deviceAuth.AuthenticateAsync();
+                }
+                else
+                {
+                    result = new FingerprintAuthenticationResult
+                    {
+                        Status = FingerprintAuthenticationResultStatus.NotAvailable,
+                        ErrorMessage = "Fallback authentication not available"
+                    };
+                }
+            }
+
+            return result;
         }
 
         public abstract Task<FingerprintAuthenticationResult> AuthenticateNoDialogAsync(IAuthenticationFailedListener failedListener, CancellationToken cancellationToken);
