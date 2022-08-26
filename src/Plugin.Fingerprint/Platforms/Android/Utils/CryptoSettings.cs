2cc9346a03f1f4377eb6a338a60e2b076678067a
DarkIrata <git@ipmix.de>
2022-08-26 17:56:43 +0200

    WIP New Settings for CryptoSettings

--- src/Plugin.Fingerprint/Platforms/Android/Utils/CryptoSettings.cs
+++ src/Plugin.Fingerprint/Platforms/Android/Utils/CryptoSettings.cs
@@ -9,6 +9,16 @@ namespace Plugin.Fingerprint.Platforms.Android.Utils
         public byte[] CipherSecretBytes { get; }
 
         /// <summary>
+        /// When disabled, manipulating the CryptoObject when verifying the result would valid and return a successful authorization 
+        /// </summary>
+        public bool EnforceCryptoObject { get; set; } = true;
+
+        /// <summary>
+        /// Validate the resulting Cipher to the provided one. CryptoObject can be changed as long as we result in the same valid cipher
+        /// </summary>
+        public bool ValidatedCipher { get; set; } = true;
+
+        /// <summary>
         /// Data used for en-/decryption of authentication data
         /// </summary>
         /// <param name="cryptoKeyName">Key Name under which the key gets stored. Should be unique to the app</param>
