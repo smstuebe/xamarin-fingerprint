70e41fca5bf79cf79b23abee4c779ccc8386437d
DarkIrata <git@ipmix.de>
2022-08-26 17:55:55 +0200

    Changed Key Name for Android Sample

--- src/Sample.Android/MainActivity.cs
+++ src/Sample.Android/MainActivity.cs
@@ -12,7 +12,7 @@ namespace Sample.Droid
         protected override void OnCreate(Bundle savedInstanceState)
         {
             CrossFingerprint.SetCurrentActivityResolver(() => this);
-            CrossFingerprint.CryptoSettings = new CryptoSettings(Application.Context.PackageName + "_bio_authentication_key", new byte[] { 11, 15, 6, 8, 8, 242, 121, 21, 100, 4, 51, 83, 95, 45, 33, 122 });
+            CrossFingerprint.CryptoSettings = new CryptoSettings(Application.Context.PackageName + "_biometric_authentication_key", new byte[] { 11, 15, 6, 8, 8, 242, 121, 21, 100, 4, 51, 83, 95, 45, 33, 122 });
 
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
